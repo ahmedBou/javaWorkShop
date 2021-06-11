e008865cc7cc31fd606f78eaf88254b4e79527ea
ahmed boutayeb <instinctauto@gmail.com>
2021-06-11 19:20:17 +0100

    odd prime palindrome with lambda expression

---
+++ myJavaWorkShop/src/com/company/GtxEdx/part2/module6/algorithme/SelectionSort.java
@@ -0,0 +1,11 @@
+package com.company.GtxEdx.part2.module6.algorithme;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] list){
+        int minIndex;
+        int nextSmallest;
+        
+
+    }
+}
